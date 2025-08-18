d7228a76b6b66345aa36b412fe73807da5cec39a
Xepobopa <ismartsdn4477@gmail.com>
2025-08-18 12:50:32 +0200

    feat: show api keys in generator header

--- internal/tui/pages/generator/header.go
+++ internal/tui/pages/generator/header.go
@@ -1,13 +1,35 @@
 package page_generator
 
 import (
+	"fmt"
+
+	"github.com/Lazy-Parser/Collector/config"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-type header struct{}
+func makeBold(str string) string {
+	return lipgloss.NewStyle().Bold(true).Render(str)
+}
 
-func NewHeader() tea.Model { return &header{} }
+type header struct {
+	cfg *config.Config
+}
 
-func (h *header) Init() tea.Cmd                            { return nil }
-func (h *header) Update(msg tea.Msg) (tea.Model, tea.Cmd)  { return h, nil }
-func (h *header) View() string                             { return "Header" }
+func NewHeader(cfg *config.Config) tea.Model {
+	return &header{cfg: cfg}
+}
+
+func (h *header) Init() tea.Cmd                           { return nil }
+func (h *header) Update(msg tea.Msg) (tea.Model, tea.Cmd) { return h, nil }
+func (h *header) View() string {
+	var str string
+
+	str += "Mexc:\n"
+	str += fmt.Sprintf("  access   %s (no limit)\n", makeBold(h.cfg.Mexc.ACCESS_TOKEN))
+	str += fmt.Sprintf("  private  %s (no limit)\n", makeBold(h.cfg.Mexc.PRIVATE_TOKEN))
+	str += "\n"
+	str += fmt.Sprintf("Coingecko: %s (watch limits / usage on https://www.coingecko.com/developers/dashboard)", makeBold(h.cfg.Coingecko.API.KEY))
+
+	return lipgloss.NewStyle().Align(lipgloss.Left).Render(str)
+}
