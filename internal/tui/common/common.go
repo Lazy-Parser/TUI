7bf06df0307230b980051814e45bdb637efbabe7
Xepobopa <ismartsdn4477@gmail.com>
2025-08-31 23:22:15 +0200

    ref: create separate package for frequently used staff

---
+++ internal/tui/common/common.go
@@ -0,0 +1,26 @@
+package common
+
+import (
+	"reflect"
+
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func CmdHandler(msg tea.Msg) tea.Cmd {
+	return func() tea.Msg {
+		return msg
+	}
+}
+
+func KeyMapToSlice(t any) (bindings []key.Binding) {
+	typ := reflect.TypeOf(t)
+	if typ.Kind() != reflect.Struct {
+		return nil
+	}
+	for i := 0; i < typ.NumField(); i++ {
+		v := reflect.ValueOf(t).Field(i)
+		bindings = append(bindings, v.Interface().(key.Binding))
+	}
+	return
+}
