87a2dead9b0935a33f8fb8d5e6e2293fedb28910
Xepobopa <ismartsdn4477@gmail.com>
2025-08-31 23:25:04 +0200

    feat: add system of background tasks

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -1,65 +1,43 @@
 package tui
 
 import (
-	"context"
 	"fmt"
-	"log"
-	"time"
 
-	"github.com/Lazy-Parser/Collector/chains"
-	"github.com/Lazy-Parser/Collector/config"
-	"github.com/Lazy-Parser/Collector/market"
-
+	"github.com/Lazy-Parser/TUI/internal/task"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-func Run(tokenRepo market.TokenRepo, cfg *config.Config, chainsService *chains.Chains) error {
+// TODO: add cancelation function for each task
+func Run() error {
 	f := StartLogger()
 
-	// try to create test tokens
-	ctx := context.Background()
-	tokens := []market.Token{
-		market.Token{
-			Name:        "Trump",
-			Decimal:     8,
-			Address:     "6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPN",
-			Image_url:   "https://dd.dexscreener.com/ds-data/tokens/solana/6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPN/header.png?key=f02e9e",
-			WithdrawFee: "10",
-			CreateTime:  time.Now().UnixMilli(),
-			Network:     "Solana",
-		},
-		market.Token{
-			Name:        "USDT",
-			Decimal:     16,
-			Address:     "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
-			Image_url:   "",
-			WithdrawFee: "5",
-			CreateTime:  time.Now().UnixMilli(),
-			Network:     "Ethereum",
-		},
-		market.Token{
-			Name:        "BR",
-			Decimal:     10,
-			Address:     "0xFf7d6A96ae471BbCD7713aF9CB1fEeB16cf56B41",
-			Image_url:   "https://dd.dexscreener.com/ds-data/tokens/bsc/0xff7d6a96ae471bbcd7713af9cb1feeb16cf56b41/header.png?key=1a07a9",
-			WithdrawFee: "20",
-			CreateTime:  time.Now().UnixMilli(),
-			Network:     "BSC",
-		},
-	}
+	manager := task.CreateTaskManager()
+	p := tea.NewProgram(InitLayout(manager), tea.WithAltScreen())
+	ch := runManager(p, manager)
 
-	for _, token := range tokens {
-		if err := tokenRepo.Save(ctx, token); err != nil {
-			log.Println(err)
-		}
-	}
-
-	p := tea.NewProgram(InitLayout(tokenRepo, cfg, chainsService), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
+		manager.Stop()
+		close(ch)
 		f.Close()
 		return fmt.Errorf("Alas, there's been an error: %v", err)
 	}
 
+	manager.Stop()
+	close(ch)
 	f.Close()
 	return nil
+}
+
+func runManager(p *tea.Program, manager *task.TaskManager) chan tea.Msg {
+	ch := make(chan tea.Msg, 1024)
+
+	// execute all
+	go manager.Run(ch)
+	go func() {
+		for msg := range ch {
+			p.Send(msg)
+		}
+	}()
+
+	return ch
 }
